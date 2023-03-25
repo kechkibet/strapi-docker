bf80bc16ae7e306de04d54e658b0754346b2328a
kech kibet <kech@ol-digital.com>
2023-03-25 22:08:12 +0300

    add: graphql

--- config/plugins.js
+++ config/plugins.js
@@ -13,5 +13,16 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  // ...
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
 });
